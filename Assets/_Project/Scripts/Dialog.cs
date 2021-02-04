28115be09c360643d9fa9f9e4a52d80243103f46
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-02-04 16:01:45 +0100

    Adding rotating dialog lines

--- Assets/_Project/Scripts/Dialog.cs
+++ Assets/_Project/Scripts/Dialog.cs
@@ -1,17 +1,56 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Dialog : MonoBehaviour
 {
-    [SerializeField] private DialogLine[] _lines;
-
+    [SerializeField] private Transform _lineUIContainer;
     [SerializeField] private Vector2 _uiPosition = new Vector2(0, 82);
     
+    [SerializeField] private DialogLine[] _lines;
+
+    private int _index = 0;
+    private DialogLineUI _lineUI;
+
     private void Awake()
     {
-        var ui = Instantiate(_lines[0].UI, transform, true);
-        ui.transform.localPosition = _uiPosition;
+        EmptyLineUIContainer();
+        NextLine();
+    }
 
-        ui.SetLine(_lines[0].Line);
+    private void EmptyLineUIContainer()
+    {
+        foreach (Transform child in _lineUIContainer)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (_index < _lines.Length)
+        {
+            RemoveCurrentLine();
+            ShowNextLine();
+            _index += 1;
+        }
+    }
+
+    private void RemoveCurrentLine()
+    {
+        if (_lineUI)
+        {
+            Destroy(_lineUI.gameObject);
+        }
+    }
+
+    private void ShowNextLine()
+    {
+        var line = _lines[_index];
+        
+        _lineUI = Instantiate(line.UI, _lineUIContainer, true);
+        var lineUITransform = _lineUI.transform;
+        lineUITransform.localPosition = _uiPosition;
+        lineUITransform.localScale = Vector3.one;
+
+        _lineUI.SetLine(line.Line);
     }
 }
