7b0539ca328fbc08f4143c8b0f361291c2f471b1
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-02-05 18:30:10 +0100

    Adding number formatting

--- Assets/_Project/Scripts/Values.cs
+++ Assets/_Project/Scripts/Values.cs
@@ -3,5 +3,6 @@
 public class Values : MonoBehaviour
 {
     public readonly string Character = "Tinka";
-    public readonly int ComboPointCount = 2;
+    public readonly int ComboPointCount = 200;
+    public readonly float StolenAmount = 1014.99f;
 }
