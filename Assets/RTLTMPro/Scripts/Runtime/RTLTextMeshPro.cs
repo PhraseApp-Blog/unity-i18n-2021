f5a7ee2766e1f1be3b808d5280c3fa8eb72c30fd
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-02-05 14:41:33 +0100

    Adding RTLTMPro and basic Arabic translation

---
+++ Assets/RTLTMPro/Scripts/Runtime/RTLTextMeshPro.cs
@@ -0,0 +1,142 @@
+﻿//#define RTL_OVERRIDE
+
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace RTLTMPro
+{
+    [ExecuteInEditMode]
+    public class RTLTextMeshPro : TextMeshProUGUI
+    {
+        // ReSharper disable once InconsistentNaming
+#if RTL_OVERRIDE
+        public override string text
+#else
+        public new string text
+#endif
+        {
+            get { return base.text; }
+            set
+            {
+                if (originalText == value)
+                    return;
+
+                originalText = value;
+
+                UpdateText();
+            }
+        }
+
+        public string OriginalText
+        {
+            get { return originalText; }
+        }
+
+        public bool PreserveNumbers
+        {
+            get { return preserveNumbers; }
+            set
+            {
+                if (preserveNumbers == value)
+                    return;
+
+                preserveNumbers = value;
+                havePropertiesChanged = true;
+            }
+        }
+
+        public bool Farsi
+        {
+            get { return farsi; }
+            set
+            {
+                if (farsi == value)
+                    return;
+
+                farsi = value;
+                havePropertiesChanged = true;
+            }
+        }
+
+        public bool FixTags
+        {
+            get { return fixTags; }
+            set
+            {
+                if (fixTags == value)
+                    return;
+
+                fixTags = value;
+                havePropertiesChanged = true;
+            }
+        }
+
+        protected bool ForceFix
+        {
+            get { return forceFix; }
+            set
+            {
+                if (forceFix == value)
+                    return;
+
+                forceFix = value;
+                havePropertiesChanged = true;
+            }
+        }
+
+        [SerializeField]
+        protected bool preserveNumbers;
+
+        [SerializeField]
+        protected bool farsi = true;
+
+        [SerializeField]
+        [TextArea(3, 10)]
+        protected string originalText;
+
+        [SerializeField]
+        protected bool fixTags = true;
+
+        [SerializeField]
+        protected bool forceFix;
+
+        protected void Update()
+        {
+            if (havePropertiesChanged)
+            {
+                UpdateText();
+            }
+        }
+
+        public void UpdateText()
+        {
+            if (originalText == null)
+                originalText = "";
+
+            if (ForceFix == false && RTLSupport.IsRTLInput(originalText) == false)
+            {
+                isRightToLeftText = false;
+                base.text = originalText;
+            }
+            else
+            {
+                isRightToLeftText = true;
+                base.text = GetFixedText(originalText);
+            }
+
+            havePropertiesChanged = true;
+        }
+
+        private string GetFixedText(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            input = RTLSupport.FixRTL(input, fixTags, preserveNumbers, farsi);
+            input = input.Reverse().ToArray().ArrayToString();
+
+            return input;
+        }
+    }
+}
