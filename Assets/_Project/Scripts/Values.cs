e88dbfbf6d0c987dfba6cdeb6132d983fd95eba6
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-02-05 18:49:00 +0100

    Adding date formatting

--- Assets/_Project/Scripts/Values.cs
+++ Assets/_Project/Scripts/Values.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Values : MonoBehaviour
 {
     public readonly string Character = "Tinka";
     public readonly int ComboPointCount = 200;
     public readonly float StolenAmount = 1014.99f;
+    public readonly DateTime Today = DateTime.Now;
 }
