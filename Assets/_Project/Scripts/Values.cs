515d0ff2359db139282d287622ee0d652759384e
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-02-05 14:29:44 +0100

    Updating translations

--- Assets/_Project/Scripts/Values.cs
+++ Assets/_Project/Scripts/Values.cs
@@ -3,5 +3,5 @@
 public class Values : MonoBehaviour
 {
     public readonly string Character = "Tinka";
-    public readonly int ComboPointCount = 112;
+    public readonly int ComboPointCount = 0;
 }
